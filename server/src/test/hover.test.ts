5563d17469372df232105244797d1ee8a1a0dacf
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-09-25 17:44:42 +0300

    Add hover tests

---
+++ server/src/test/hover.test.ts
@@ -0,0 +1,45 @@
+import { Position, Range } from "vscode-languageserver";
+import { Test } from "./test";
+
+suite("Hover tests", () => {
+    [
+        new Test(
+            "Hover place is calculated properly",
+            `[configuration]
+  entity-expression = cpu_busy`,
+            {
+                contents: "Apply server-side filter to all series based on entity names, tags, and fields.",
+                range: Range.create(1, "  ".length, 1, "  ".length + "entity-expression".length),
+            },
+            undefined,
+            Position.create(1, "  ent".length),
+        ),
+        new Test(
+            "Hover is not provided for a value",
+            `[configuration]
+  entity-expression = cpu_busy`,
+            null,
+            undefined,
+            Position.create(1, "  entity-expression = c".length),
+        ),
+        new Test(
+            "Hover is not provided for a section",
+            `[configuration]
+  entity-expression = cpu_busy`,
+            null,
+            undefined,
+            Position.create(0, "[conf".length),
+        ),
+        new Test(
+            "Hover is provided for a setting containing whitespace",
+            `[configuration]
+  entity expression = cpu_busy`,
+            {
+                contents: "Apply server-side filter to all series based on entity names, tags, and fields.",
+                range: Range.create(1, "  ".length, 1, "  ".length + "entity expression".length),
+            },
+            undefined,
+            Position.create(0, "  entity expr".length),
+        ),
+    ].forEach((test: Test): void => test.hoverTest());
+});
