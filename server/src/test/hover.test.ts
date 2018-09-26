4ad33e9e55d0efe3d72e2edb2a3a96bc498e0120
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-09-26 10:19:49 +0300

    Add new test

--- server/src/test/hover.test.ts
+++ server/src/test/hover.test.ts
@@ -41,5 +41,16 @@ suite("Hover tests", () => {
             undefined,
             Position.create(1, "  entity expr".length),
         ),
+        new Test(
+            "Hover is provided if a space between setting name and equals sign is absent",
+            `[configuration]
+  entity expression= cpu_busy`,
+            {
+                contents: "Apply server-side filter to all series based on entity names, tags, and fields.",
+                range: Range.create(1, "  ".length, 1, "  ".length + "entity expression".length),
+            },
+            undefined,
+            Position.create(1, "  entity expr".length),
+        ),
     ].forEach((test: Test): void => test.hoverTest());
 });
