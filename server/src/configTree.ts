a637a1f80d45532f29aba3945054e7cfb0dba76b
PolinaShneider <beatrixlinkoln@gmail.com>
2019-05-08 16:52:16 +0300

    Move relatedSettings, checkColorsMatchTreshold and checkTimeSettings to separate file

--- server/src/configTree.ts
+++ server/src/configTree.ts
@@ -1,158 +1,14 @@
 import { Diagnostic, DiagnosticSeverity } from "vscode-languageserver";
-import { incorrectColors, uselessScope } from "./messageUtil";
+import { uselessScope } from "./messageUtil";
 import {
     Condition, Requirement,
-    sectionMatchConditionRequired, sectionMatchConditionUseless,
     SectionScope
 } from "./requirement";
+import { checkColorsMatchTreshold, checkTimeSettings, relatedSettings } from "./requirementsChecker";
 import { isNestedToPrevious, sectionDepthMap } from "./resources";
 import { Setting } from "./setting";
 import { TextRange } from "./textRange";
 import { createDiagnostic, getSetting } from "./util";
-
-/**
- * If `requiredIfConditions` !== null OR `requiredAnyIfConditions` !== null,
- * the section will be checked for match to `conditions`; if section matches conditions, then:
- *  1) setting, specified in `requiredIfConditions` is required for this section;
- *  2) required at least one setting from `requiredAnyIfConditions`.
- * `requiredIfConditions` can not be specified simultaneously with `requiredAnyIfConditions`.
- *
- * If `requiredIfConditions` == null AND `requiredAnyIfConditions` == null,
- * the section will be checked for applicability of any of `dependent`;
- * if any setting from `dependent` is declared in the section, then section will be checked for match to `conditions`.
- */
-const relatedSettings: Requirement[] = [
-    {
-        /**
-         * If "type" is "calendar", "treemap " or "gauge" and mode is "half" or "default",
-         * "colors" and "thresholds" are applicable, and if "colors" are specified, the "thresholds" are required.
-         */
-        conditions: [
-            sectionMatchConditionRequired("type", ["calendar", "treemap", "gauge"]),
-            sectionMatchConditionRequired("mode", ["half", "default"])
-        ],
-        dependent: "colors",
-        requiredIfConditions: "thresholds"
-    },
-    {
-        conditions: [
-            sectionMatchConditionRequired("type", ["chart"]),
-            sectionMatchConditionRequired("mode", ["column", "column-stack"])
-        ],
-        dependent: "forecast-style",
-        requiredIfConditions: "data-type"
-    },
-    {
-        conditions: [
-            sectionMatchConditionRequired("type", ["chart"])
-        ],
-        dependent: "forecast-ssa-group-auto-count",
-        requiredIfConditions: "forecast-ssa-decompose-eigentriple-limit"
-    },
-    {
-        /**
-         * If "type=chart" and "forecast-horizon-start-time" is specified,
-         * any of "forecast-horizon-end-time", "forecast-horizon-interval",
-         * "forecast-horizon-length" is required in [series].
-         */
-        conditions: [
-            sectionMatchConditionRequired("type", ["chart"])
-        ],
-        dependent: "forecast-horizon-start-time",
-        requiredAnyIfConditions: ["forecast-horizon-end-time", "forecast-horizon-interval", "forecast-horizon-length"]
-    },
-    {
-        /**
-         * If "end-time" is specified,
-         * consider it is less than "forecast-horizon-end-time"
-         */
-        dependent: "end-time",
-        relation: "forecast-horizon-end-time"
-    },
-    {
-        /**
-         * If "start-time" is specified,
-         * consider it is less than "end-time"
-         */
-        dependent: "start-time",
-        relation: "end-time"
-    },
-    {
-        /**
-         * If "type=chart" and "mode" is NOT "column-stack" or "column",
-         * settings "negative-style" and "current-period-style" are useless.
-         */
-        conditions: [
-            sectionMatchConditionUseless("type", ["chart"]),
-            sectionMatchConditionUseless("mode", ["column-stack", "column"])
-        ],
-        dependent: ["negative-style", "current-period-style"]
-    },
-    {
-        conditions: [
-            sectionMatchConditionUseless("type", ["chart"]),
-            sectionMatchConditionUseless("server-aggregate", ["false"])
-        ],
-        dependent: "moving-average"
-    },
-    {
-        conditions: [
-            sectionMatchConditionUseless("type", ["calendar", "treemap", "gauge"]),
-            sectionMatchConditionUseless("mode", ["half", "default"])
-        ],
-        dependent: ["ticks", "color-range", "gradient-count"]
-    },
-    {
-        conditions: [
-            sectionMatchConditionUseless("type", ["chart"]),
-            sectionMatchConditionUseless("forecast-arima-auto", ["false"])
-        ],
-        dependent: ["forecast-arima-auto-regression-interval", "forecast-arima-d", "forecast-arima-p"]
-    },
-    {
-        conditions: [
-            sectionMatchConditionUseless("type", ["chart"]),
-            sectionMatchConditionUseless("forecast-hw-auto", ["false"])
-        ],
-        dependent: ["forecast-hw-alpha", "forecast-hw-beta", "forecast-hw-gamma"]
-    },
-    {
-        dependent: "table", requiredIfConditions: "attribute"
-    },
-    {
-        dependent: "attribute", requiredIfConditions: "table"
-    },
-    {
-        dependent: "column-alert-style", requiredIfConditions: "column-alert-expression"
-    },
-    {
-        dependent: "alert-style", requiredIfConditions: "alert-expression"
-    },
-    {
-        dependent: "link-alert-style", requiredIfConditions: "alert-expression"
-    },
-    {
-        dependent: "node-alert-style", requiredIfConditions: "alert-expression"
-    },
-    {
-        dependent: "icon-alert-style", requiredIfConditions: "icon-alert-expression"
-    },
-    {
-        dependent: "icon-alert-expression", requiredIfConditions: "icon"
-    },
-    {
-        dependent: "icon-color", requiredIfConditions: "icon"
-    },
-    {
-        dependent: "icon-position", requiredIfConditions: "icon"
-    },
-    {
-        dependent: "icon-size", requiredIfConditions: "icon"
-    },
-    {
-        dependent: "caption-style", requiredIfConditions: "caption"
-    }
-];
 
 class Section {
 
@@ -342,27 +198,6 @@ export class ConfigTree {
     }
 
     /**
-     * Checks that start point isn't greater than end point
-     * @param start interval start point
-     * @param end interval end point
-     */
-    public checkTimeSettings(start?: Setting, end?: Setting): void {
-        if (end === undefined || start === undefined) {
-            return;
-        }
-
-        if (start.value >= end.value) {
-            this.diagnostics.push(
-                createDiagnostic(
-                    end.textRange,
-                    `${end.displayName} must be greater than ${start.displayName}`,
-                    DiagnosticSeverity.Error
-                )
-            );
-        }
-    }
-
-    /**
      * Returns object from relatedSettings based on setting.displayName.
      * @param settingName setting.displayName
      */
@@ -423,7 +258,13 @@ export class ConfigTree {
                     switch (required) {
                         case "thresholds": {
                             const colorsSetting = ConfigTree.getSetting(section, "colors");
-                            this.checkColorsMatchTreshold(colorsSetting, checkedSetting);
+
+                            const colorsDontMatch = checkColorsMatchTreshold(colorsSetting, checkedSetting);
+
+                            if (colorsDontMatch !== undefined) {
+                                this.colorsDiagnostics.set(colorsSetting, colorsDontMatch);
+                            }
+
                             break;
                         }
                         case "forecast-ssa-decompose-eigentriple-limit": {
@@ -465,7 +306,13 @@ export class ConfigTree {
                         }
                     }
 
-                    this.checkTimeSettings(start, end);
+                    const checkTimeWarning = checkTimeSettings(start, end);
+
+                    if (checkTimeWarning !== undefined) {
+                        this.diagnostics.push(
+                            checkTimeWarning
+                        );
+                    }
                 }
             }
         }
@@ -482,39 +329,6 @@ export class ConfigTree {
                 uselessScope(dependent.displayName, `${msg.join(", ")}`),
                 DiagnosticSeverity.Warning
             ));
-        }
-    }
-
-    /**
-     * Check the relationship between thresholds and colors:
-     * in "gauge", "calendar", "treemap" number of colors (if specified) must be equal to number of thresholds minus 1.
-     */
-    private checkColorsMatchTreshold(colorsSetting: Setting, thresholdsSetting: Setting) {
-        let colorsValues;
-        let thresholdsValues;
-        if (colorsSetting.values.length > 0) {
-            colorsSetting.values.push(colorsSetting.value);
-            colorsValues = colorsSetting.values;
-        } else {
-            /**
-             * Converts 1) -> 2):
-             * 1) colors = rgb(247,251,255), rgb(222,235,247), rgb(198,219,239)
-             * 2) colors = rgb, rgb, rgb
-             */
-            colorsValues = colorsSetting.value.replace(/(\s*\d{3}\s*,?)/g, "");
-            colorsValues = colorsValues.split(",").filter(s => s.trim() !== "");
-        }
-        if (thresholdsSetting.values.length > 0) {
-            thresholdsSetting.values.push(thresholdsSetting.value);
-            thresholdsValues = thresholdsSetting.values;
-        } else {
-            thresholdsValues = thresholdsSetting.value.split(",").filter(s => s.trim() !== "");
-        }
-
-        const expected = thresholdsValues.length - 1;
-        if (colorsValues.length !== expected) {
-            this.colorsDiagnostics.set(colorsSetting, createDiagnostic(colorsSetting.textRange,
-                incorrectColors(`${colorsValues.length}`, `${expected}`)));
         }
     }
 
---
+++ server/src/requirementsChecker.ts
@@ -0,0 +1,214 @@
+import { Diagnostic, DiagnosticSeverity } from "vscode-languageserver";
+import { incorrectColors } from "./messageUtil";
+import {
+    Requirement,
+    sectionMatchConditionRequired,
+    sectionMatchConditionUseless
+} from "./requirement";
+import { Setting } from "./setting";
+import { createDiagnostic } from "./util";
+
+/**
+ * Related settings requirements
+ *
+ * If `requiredIfConditions` !== null OR `requiredAnyIfConditions` !== null,
+ * the section will be checked for match to `conditions`; if section matches conditions, then:
+ *  1) setting, specified in `requiredIfConditions` is required for this section;
+ *  2) required at least one setting from `requiredAnyIfConditions`.
+ * `requiredIfConditions` can not be specified simultaneously with `requiredAnyIfConditions`.
+ *
+ * If `requiredIfConditions` == null AND `requiredAnyIfConditions` == null,
+ * the section will be checked for applicability of any of `dependent`;
+ * if any setting from `dependent` is declared in the section, then section will be checked for match to `conditions`.
+ */
+export const relatedSettings: Requirement[] = [
+    {
+        /**
+         * If "type" is "calendar", "treemap " or "gauge" and mode is "half" or "default",
+         * "colors" and "thresholds" are applicable, and if "colors" are specified, the "thresholds" are required.
+         */
+        conditions: [
+            sectionMatchConditionRequired("type", ["calendar", "treemap", "gauge"]),
+            sectionMatchConditionRequired("mode", ["half", "default"])
+        ],
+        dependent: "colors",
+        requiredIfConditions: "thresholds"
+    },
+    {
+        conditions: [
+            sectionMatchConditionRequired("type", ["chart"]),
+            sectionMatchConditionRequired("mode", ["column", "column-stack"])
+        ],
+        dependent: "forecast-style",
+        requiredIfConditions: "data-type"
+    },
+    {
+        conditions: [
+            sectionMatchConditionRequired("type", ["chart"])
+        ],
+        dependent: "forecast-ssa-group-auto-count",
+        requiredIfConditions: "forecast-ssa-decompose-eigentriple-limit"
+    },
+    {
+        /**
+         * If "type=chart" and "forecast-horizon-start-time" is specified,
+         * any of "forecast-horizon-end-time", "forecast-horizon-interval",
+         * "forecast-horizon-length" is required in [series].
+         */
+        conditions: [
+            sectionMatchConditionRequired("type", ["chart"])
+        ],
+        dependent: "forecast-horizon-start-time",
+        requiredAnyIfConditions: ["forecast-horizon-end-time", "forecast-horizon-interval", "forecast-horizon-length"]
+    },
+    {
+        /**
+         * If "end-time" is specified,
+         * consider it is less than "forecast-horizon-end-time"
+         */
+        dependent: "end-time",
+        relation: "forecast-horizon-end-time"
+    },
+    {
+        /**
+         * If "start-time" is specified,
+         * consider it is less than "end-time"
+         */
+        dependent: "start-time",
+        relation: "end-time"
+    },
+    {
+        /**
+         * If "type=chart" and "mode" is NOT "column-stack" or "column",
+         * settings "negative-style" and "current-period-style" are useless.
+         */
+        conditions: [
+            sectionMatchConditionUseless("type", ["chart"]),
+            sectionMatchConditionUseless("mode", ["column-stack", "column"])
+        ],
+        dependent: ["negative-style", "current-period-style"]
+    },
+    {
+        conditions: [
+            sectionMatchConditionUseless("type", ["chart"]),
+            sectionMatchConditionUseless("server-aggregate", ["false"])
+        ],
+        dependent: "moving-average"
+    },
+    {
+        conditions: [
+            sectionMatchConditionUseless("type", ["calendar", "treemap", "gauge"]),
+            sectionMatchConditionUseless("mode", ["half", "default"])
+        ],
+        dependent: ["ticks", "color-range", "gradient-count"]
+    },
+    {
+        conditions: [
+            sectionMatchConditionUseless("type", ["chart"]),
+            sectionMatchConditionUseless("forecast-arima-auto", ["false"])
+        ],
+        dependent: ["forecast-arima-auto-regression-interval", "forecast-arima-d", "forecast-arima-p"]
+    },
+    {
+        conditions: [
+            sectionMatchConditionUseless("type", ["chart"]),
+            sectionMatchConditionUseless("forecast-hw-auto", ["false"])
+        ],
+        dependent: ["forecast-hw-alpha", "forecast-hw-beta", "forecast-hw-gamma"]
+    },
+    {
+        dependent: "table", requiredIfConditions: "attribute"
+    },
+    {
+        dependent: "attribute", requiredIfConditions: "table"
+    },
+    {
+        dependent: "column-alert-style", requiredIfConditions: "column-alert-expression"
+    },
+    {
+        dependent: "alert-style", requiredIfConditions: "alert-expression"
+    },
+    {
+        dependent: "link-alert-style", requiredIfConditions: "alert-expression"
+    },
+    {
+        dependent: "node-alert-style", requiredIfConditions: "alert-expression"
+    },
+    {
+        dependent: "icon-alert-style", requiredIfConditions: "icon-alert-expression"
+    },
+    {
+        dependent: "icon-alert-expression", requiredIfConditions: "icon"
+    },
+    {
+        dependent: "icon-color", requiredIfConditions: "icon"
+    },
+    {
+        dependent: "icon-position", requiredIfConditions: "icon"
+    },
+    {
+        dependent: "icon-size", requiredIfConditions: "icon"
+    },
+    {
+        dependent: "caption-style", requiredIfConditions: "caption"
+    }
+];
+
+/**
+ * Checks that setting start point isn't greater than end point
+ * @param start interval start point
+ * @param end interval end point
+ */
+export const checkTimeSettings = (start?: Setting, end?: Setting): Diagnostic | undefined  => {
+    if (end === undefined || start === undefined) {
+        return undefined;
+    }
+
+    if (start.value >= end.value) {
+        return createDiagnostic(
+            end.textRange,
+            `${end.displayName} must be greater than ${start.displayName}`,
+            DiagnosticSeverity.Error
+        );
+    }
+
+    return undefined;
+};
+
+/**
+ * Check the relationship between thresholds and colors:
+ * in "gauge", "calendar", "treemap" number of colors (if specified) must be equal to number of thresholds minus 1.
+ */
+export const checkColorsMatchTreshold = (
+    colorsSetting: Setting,
+    thresholdsSetting: Setting
+): Diagnostic | undefined => {
+    let colorsValues;
+    let thresholdsValues;
+    if (colorsSetting.values.length > 0) {
+        colorsSetting.values.push(colorsSetting.value);
+        colorsValues = colorsSetting.values;
+    } else {
+        /**
+         * Converts 1) -> 2):
+         * 1) colors = rgb(247,251,255), rgb(222,235,247), rgb(198,219,239)
+         * 2) colors = rgb, rgb, rgb
+         */
+        colorsValues = colorsSetting.value.replace(/(\s*\d{3}\s*,?)/g, "");
+        colorsValues = colorsValues.split(",").filter(s => s.trim() !== "");
+    }
+    if (thresholdsSetting.values.length > 0) {
+        thresholdsSetting.values.push(thresholdsSetting.value);
+        thresholdsValues = thresholdsSetting.values;
+    } else {
+        thresholdsValues = thresholdsSetting.value.split(",").filter(s => s.trim() !== "");
+    }
+
+    const expected = thresholdsValues.length - 1;
+
+    if (colorsValues.length !== expected) {
+        return createDiagnostic(colorsSetting.textRange, incorrectColors(`${colorsValues.length}`, `${expected}`));
+    } else {
+        return undefined;
+    }
+};
