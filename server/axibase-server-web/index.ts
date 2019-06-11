ccd3c1cbd034e8ae25cd7eae2fdf81e7d4dd5aaf
PolinaShneider <beatrix.linkoln@gmail.com>
2019-06-11 11:13:14 +0300

    index.ts -> index.js

--- server/axibase-server-web/index.ts
+++
@@ -1,7 +0,0 @@
-import { CompletionProvider } from "./dist/server/src/completionProvider";
-import { Validator } from "./dist/server/src/validator";
-
-export class Server {
-    public static CompletionProvider: CompletionProvider;
-    public static Validator: Validator;
-}
