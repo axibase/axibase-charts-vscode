0804ab80e3242063670a5b348d744831d31b4541
PolinaShneider <beatrix.linkoln@gmail.com>
2019-06-11 11:07:51 +0300

    Change entry file

--- server/axibase-server-web/index.ts
+++ server/axibase-server-web/index.ts
@@ -1,7 +1,7 @@
 import { CompletionProvider } from "./dist/server/src/completionProvider";
 import { Validator } from "./dist/server/src/validator";
 
-export const server = {
-    CompletionProvider,
-    Validator
-};
+export class Server {
+    public static CompletionProvider: CompletionProvider;
+    public static Validator: Validator;
+}
