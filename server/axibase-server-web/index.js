2b8d2a567d10c876cb076f8ce74a4578d5ee018e
PolinaShneider <beatrix.linkoln@gmail.com>
2019-06-11 10:49:05 +0300

    Add index.js entry point

---
+++ server/axibase-server-web/index.js
@@ -0,0 +1,7 @@
+import { CompletionProvider } from "./dist/server/src/completionProvider";
+import { Validator } from "./dist/server/src/validator";
+
+export const server = {
+    CompletionProvider,
+    Validator
+};
