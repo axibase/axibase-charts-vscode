23eb28e8428e34318c2af8fa3fee53cf7289d063
PolinaShneider <beatrix.linkoln@gmail.com>
2019-06-11 18:45:22 +0300

    Remove AMD comment

--- server/src/checkPriority.ts
+++ server/src/checkPriority.ts
@@ -1,7 +1,6 @@
 /**
  * Used in JavaScriptChecksQueue to ensure that the udf is placed earlier than it's first call
  */
-/// <amd-module name="CheckPriority"/>
 export const enum CheckPriority {
     High,
     Low
