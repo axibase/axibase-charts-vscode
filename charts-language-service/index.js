514657ec9af624dce80233556f96167263d725d9
PolinaShneider <beatrix.linkoln@gmail.com>
2019-06-13 16:36:29 +0300

    Rename charts-language-service folder

--- charts-language-service/index.js
+++
@@ -1,5 +0,0 @@
-import { CompletionProvider } from "../server/src/completionProvider";
-import { Validator } from "../server/src/validator";
-
-export const _CompletionProvider = CompletionProvider;
-export const _Validator = Validator;
---
+++ language-service/index.js
@@ -0,0 +1,4 @@
+import { CompletionProvider } from "../server/src/completionProvider";
+import { Validator } from "../server/src/validator";
+import { ResourcesProviderWeb } from "../server/src/resourcesProviderWeb";
+export { CompletionProvider, ResourcesProviderWeb, Validator };
