05207a98828dc89ba84d5b171ea79cc8f7f9ed54
PolinaShneider <beatrix.linkoln@gmail.com>
2019-06-18 19:51:13 +0300

    Working web-version

--- language-service/index.js
+++
@@ -1,4 +0,0 @@
-import { CompletionProvider } from "../server/src/completionProvider";
-import { Validator } from "../server/src/validator";
-import { ResourcesProviderWeb } from "../server/src/resourcesProviderWeb";
-export { CompletionProvider, ResourcesProviderWeb, Validator };
