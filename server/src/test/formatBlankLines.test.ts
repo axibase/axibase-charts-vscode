101ce984e82603b30d3052de54f2a36de73ee111
PolinaShneider <beatrixlinkoln@gmail.com>
2019-07-30 13:44:11 +0300

    Add formatting test

---
+++ server/src/test/formatBlankLines.test.ts
@@ -0,0 +1,34 @@
+import { deepStrictEqual } from "assert";
+import { FormattingOptions, Position, Range, TextEdit } from "vscode-languageserver";
+import { Formatter } from "../formatter";
+
+suite("Blank lines formatting", () => {
+    test("Delete extra blank lines between sections", () => {
+        const text = `[configuration]
+  offset-right = 50
+
+
+[group]`;
+        const options: FormattingOptions = FormattingOptions.create(2, true);
+        const expected: TextEdit[] = [
+            TextEdit.replace(Range.create(
+                Position.create(2, 0),
+                Position.create(3, 0)),
+                ""
+            ),
+            TextEdit.replace(Range.create(
+                Position.create(3, 0),
+                Position.create(4, 0)),
+                ""
+            ),
+            TextEdit.replace(Range.create(
+                Position.create(4, 0),
+                Position.create(4, "[group]".length)),
+                "\n[group]"
+            )
+        ];
+        const formatter = new Formatter(text, options, true);
+        const actual = formatter.lineByLine();
+        deepStrictEqual(actual, expected);
+    });
+});
